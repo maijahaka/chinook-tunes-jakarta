5f995698ef4a6c330c2f4f7201ebd4122117dde6
Maija Haka <maija.haka@live.fi>
2025-04-01 14:32:46 +0300

    feat: add servlet to handle get requests to root

---
+++ src/main/java/org/haka/chinooktunesjakarta/servlets/RootServlet.java
@@ -0,0 +1,17 @@
+package org.haka.chinooktunesjakarta.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class RootServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().write("<h1>Welcome to Chinook Tunes!</h1>");
+    }
+}
